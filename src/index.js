5c478ac890606c8910d13449b9e78f34b86e3511
leninner <mazabandalenin180@gmail.com>
2021-09-17 15:27:27 -0500

    Añado imágenes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 let nameCity = document.getElementById('name');
 let prueba = document.getElementById('prueba');
 let btn = document.getElementById('btn');
+let imagen = document.getElementById('imgPrueba');
 
-const userId = 'a4e7013a3e412c322cba4d957d4cad8d';
+//Librería para guardar los climas
+const climas = {
+  viento: './src/assets/images/viento.png',
+  soleado: './src/assets/images/soleado.png',
+  torrencial: './src/assets/images/torrencial.png',
+  lluvia: './src/assets/images/lluvia.png',
+  llovizna: './src/assets/images/llovizna.png',
+  templado: './src/assets/images/llovizna.png',
+  nublado: './src/assets/images/nublado.png',
+};
 
-// Función para consultar los datos de las ciudades
-async function getCityInfo(cityName) {
-  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${userId}`;
-  const response = await fetch(URL, {
-    mode: 'cors',
-  });
-  const cityInfo = await response.json();
-  return cityInfo;
-}
+imagen.src = climas.soleado;
 
-function displayInfo(cityName) {
-  getCityInfo(cityName)
-    .then((data) => {
-      prueba.textContent = `${data.name} ${data.main.temp}`;
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+// const userId = 'a4e7013a3e412c322cba4d957d4cad8d';
 
-btn.addEventListener('click', () => displayInfo(nameCity.value)); //Es importante hacer una callback para funciones, ya que de caso contrario se ejecuta inmediatamente
+// // Función para consultar los datos de las ciudades
+// async function getCityInfo(cityName) {
+//   const URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${userId}`;
+//   const response = await fetch(URL, {
+//     mode: 'cors',
+//   });
+//   const cityInfo = await response.json();
+//   return cityInfo;
+// }
+
+// function displayInfo(cityName) {
+//   getCityInfo(cityName)
+//     .then((data) => {
+//       prueba.textContent = `${data.name} ${data.main.temp}`;
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+// btn.addEventListener('click', () => displayInfo(nameCity.value)); //Es importante hacer una callback para funciones, ya que de caso contrario se ejecuta inmediatamente
